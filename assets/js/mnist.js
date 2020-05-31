96bda55eb72f57eefbfa954acc82ecad04fd6e63
Deepak <patwa.deepak@gmail.com>
2020-05-31 12:39:59 +0530

    improved mobile scroll prevention using CSS

--- assets/js/mnist.js
+++ assets/js/mnist.js
@@ -106,23 +106,6 @@ window.addEventListener("load", () => {
             y: touchEvent.touches[0].clientY - rect.top
         };
     }
-
-    // Prevent scrolling when touching the canvas
-    document.body.addEventListener("touchstart", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
-    document.body.addEventListener("touchend", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
-    document.body.addEventListener("touchmove", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
 });
 
 
